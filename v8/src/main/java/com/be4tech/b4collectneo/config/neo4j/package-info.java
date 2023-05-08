6b2c06119a3a463e0356eb32b1da25d649dce522
Ivan Santander <isantander@be4tech.co>
2023-05-08 10:27:13 -0500

    Initial version of v4 generated by generator-jhipster@7.9.3

---
+++ v8/src/main/java/com/be4tech/b4collectneo/config/neo4j/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Neo4j specific configuration and required migrations.
+ */
+package com.be4tech.b4collectneo.config.neo4j;
---
+++ v8/src/main/java/com/be4tech/b4collectneo/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.be4tech.b4collectneo.security;
